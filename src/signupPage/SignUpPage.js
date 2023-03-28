ee6ecb5b5a1ee0a4bf1a84e052ab79a94922a799
‘minzung’ <candy010425@gamil.com>
2023-03-28 19:49:36 +0900

    회원가입 페이지 추가

--- src/signupPage/SignUpPage.js
+++ src/signupPage/SignUpPage.js
@@ -1,128 +1,192 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import './SignUpPage.css';
+import CheckIcon from '@material-ui/icons/Check';
+import ClearIcon from '@material-ui/icons/Clear';
+import axios from 'axios';
 
-const SignUpPage = () => {
-  const [userName, setUserName] = useState("");
-  const [studentId, setStudentId] = useState("");
-  const [email, setEmail] = useState("");
-  const [passwd, setPasswd] = useState("");
-  const [checkPasswd, setCheckPasswd] = useState("");
-  const [nickName, setNickName] = useState("");
-  const move = useNavigate();
+const SingUpPage = () => {
+    const move = useNavigate();
+    
+    const [studentId, setStudentId] = useState("none");
+    const [passwd, setPasswd] = useState("none");
+    const [email, setEmail] = useState("none");
+    const [userName, setUserName] = useState("none");
+    const [nickName, setnickName] = useState("none");
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+    const [checkIdDisplay, setCheckIdDisplay] = useState("none");
+    const [clearIdDisplay, setClearIdDisplay] = useState("none");
 
-    if (userName.trim().length === 0) {
-      alert("이름을 입력하세요");
-      return;
-    } else if (passwd.trim().length === 0) {
-      alert("비밀번호를 입력하세요");
-      return;
-    } else if (email.trim().length === 0) {
-      alert("이메일을 입력하세요");
-      return;
-    } else if (nickName.trim().length === 0) {
-      alert("사용할 닉네임을 입력하세요");
-      return;
-    } else if (studentId.trim().length === 0) {
-      alert("학번을 입력하세요");
-      return;
-    } else {
-      axios
-        .post("http://localhost:8080/register", {
-          userName,
-          email,
-          studentId,
-          passwd,
-          nickName,
-        })
-        .then(() => {
-          alert("작성되었습니다!");
-          move("/login");
-        });
-    }
-  };
+    const [checkPasswdDisplay, setCheckPasswdDisplay] = useState("none");
+    const [clearPasswdDisplay, setClearPasswdDisplay]= useState("none");
 
-  const checkPw = (passwd) => {
-    if (passwd != checkPasswd) {
-      alert("비밀번호를 다시 확인해주세요");
-    }
-  };
+    const [checkConfirmpasswdDisplay, setCheckConfirmpasswdDisplay] = useState("none");
+    const [clearConfirmpasswdDisplay, setClearConfirmpasswdDisplay] = useState("none");
 
-  return (
-    <div className="lec-back p-4">
-      <h5>회원가입</h5>
-      <br />
-      <div>
-        <input
-          className="form-control"
-          value={userName}
-          onChange={(e) => {
-            setUserName(e.target.value);
-          }}
-          placeholder="이름을 입력해주세요."
-        ></input>
-        <br />
-        <input
-          className="form-control"
-          type="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-          placeholder="이메일을 입력해주세요."
-        ></input>
-        <br />
+    const [checkEmailDisplay, setCheckEmailDisplay] = useState("none");
+    const [clearEmailDisplay, setClearEmailDisplay] = useState("none");
 
-        <input
-          className="form-control"
-          value={studentId}
-          onChange={(e) => {
-            setStudentId(e.target.value);
-          }}
-          placeholder="학번을 입력해주세요."
-        ></input>
-        <br />
+    const [checkNameDisplay, setCheckNameDisplay] = useState("none");
+    const [clearNameDisplay, setClearNameDisplay] = useState("none");
 
-        <input
-          className="form-control"
-          type="password"
-          value={passwd}
-          onChange={(e) => {
+    const [checknickNameDisplay, setChecknickNameDisplay] = useState("none");
+    const [clearnickNameDisplay, setClearnickNameDisplay] = useState("none");
+
+    const studentIdCheck = (e) => {
+        try {
+            if(e.target.value.length === 7) {
+                setStudentId(e.target.value);
+                setCheckIdDisplay("block");
+                setClearIdDisplay("none");
+            } else{
+                setClearIdDisplay("block");
+                setCheckIdDisplay("none");
+            }
+        } catch(e) {
+
+        }
+    };
+
+    const passwdCheck = (e) => {
+        const regex = /^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+])[a-zA-Z0-9!@#$%^&*()_+]{8,}$/;
+
+        if(regex.test(e.target.value)) {
             setPasswd(e.target.value);
-          }}
-          placeholder="비밀번호를 입력해주세요."
-        ></input>
-        <br />
+            setCheckPasswdDisplay("block");
+            setClearPasswdDisplay("none");
+        } else {
+            setClearPasswdDisplay("block");
+            setCheckPasswdDisplay("none");
+        }
+    };
 
-        <input
-          className="form-control"
-          type="password"
-          value={checkPasswd}
-          onChange={(e) => {
-            setCheckPasswd(e.target.value);
-          }}
-          placeholder="비밀번호를 한번 더 입력해주세요."
-        ></input>
-        <br />
+    const confirmPasswdCheck = (e) => {
+        if(e.target.value !== passwd) {
+            setClearConfirmpasswdDisplay("block");
+            setCheckConfirmpasswdDisplay("none");
+        } else {
+            setCheckConfirmpasswdDisplay("block");
+            setClearConfirmpasswdDisplay("none");
+        }
+    };
 
-        <input
-          className="form-control"
-          value={nickName}
-          onChange={(e) => {
-            setNickName(e.target.value);
-          }}
-          placeholder="닉네임을 입력해주세요."
-        ></input>
-        <br />
-        <button className="lec-button mb-3" type="submit" onClick={onSubmit}>
-          회원가입 완료
-        </button>
-      </div>
-    </div>
-  );
-};
+    const emailCheck = (e) => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-export default SignUpPage;
+        try {
+            if(regex.test(e.target.value)) {
+                setEmail(e.target.value);
+                setCheckEmailDisplay("block");
+                setClearEmailDisplay("none");
+            } else {
+                setClearEmailDisplay("block");
+                setCheckEmailDisplay("none");
+            }
+        } catch(e) {
+
+        }
+    };
+
+    const nameCheck = (e) => {
+        if(e.target.value.length >= 3) {
+            setUserName(e.target.value);
+            setCheckNameDisplay("block");
+            setClearNameDisplay("none");
+        } else {
+            setClearNameDisplay("block");
+            setCheckNameDisplay("none");
+        }
+    };
+
+    const nickNameCheck = (e) => {
+        try {
+            if( e.target.value.length >= 3) {
+                setnickName(e.target.value);
+                setChecknickNameDisplay("block");
+                setClearnickNameDisplay("none");
+            } else {
+                setClearnickNameDisplay("block");
+                setChecknickNameDisplay("none");
+            }
+        } catch(e) {
+
+        }
+    };
+
+    const onSubmit = (e) => {
+      e.preventDefault();
+      
+        if(checkIdDisplay === "block" && checkPasswdDisplay === "block" && checkConfirmpasswdDisplay === "block" && checkEmailDisplay === "block" && checkNameDisplay === "block" && checknickNameDisplay === "block" ) {
+          axios.post("http://localhost:8080/register", {
+            studentId,
+            email, 
+            passwd,
+            userName,
+            nickName
+          })
+          alert(userName + "님, 회원가입을 축하합니다.");
+          move("/login")
+        } else {
+            alert("입력사항을 다시 확인해주세요.");
+        }
+        
+    };
+
+    return (
+        <div id="register-container">
+            <h2>에브리타임 회원가입</h2>
+            <p className="description">에브리타임 계정으로 <strong>캠퍼스픽, 에브리타임</strong> 등<br />다양한 대학생 서비스를 모두 이용하실 수 있습니다.</p>
+            <div className="input">
+                <div className="label"><label>학번</label></div>
+                <div className="inputbox idAvailable">
+                    <input onChange={studentIdCheck} type="text" name="user_id" maxlength="20" placeholder="아이디를 입력하세요." className="search id_search" />
+                    <CheckIcon className="checkIcon"style={{ display: checkIdDisplay }}/>    
+                    <ClearIcon className="clearIcon" style={{ display: clearIdDisplay }}/>    
+                </div>
+            </div>
+            <div className="input">
+                <div className="label"><label>비밀번호</label></div>
+                <div className="inputbox">
+                    <input onChange={passwdCheck} type="password" name="passwd" maxlength="20" placeholder="소문자, 숫자, 특수문자를 포함하여 8자 이상 입력하세요." className="search" />
+                    <CheckIcon className="checkIcon"style={{ display: checkPasswdDisplay }}/>    
+                    <ClearIcon className="clearIcon" style={{ display: clearPasswdDisplay }}/>  
+                </div>
+            </div>
+            <div className="input">
+                <div className="label"><label>비밀번호 확인</label></div>
+                <div className="inputbox">
+                    <input onChange={confirmPasswdCheck} type="password" name="user_repw" maxlength="20" placeholder="비밀번호를 다시 입력하세요." className="search" />
+                    <CheckIcon className="checkIcon"style={{ display: checkConfirmpasswdDisplay }}/>    
+                    <ClearIcon className="clearIcon" style={{ display: clearConfirmpasswdDisplay }}/>  
+                </div>
+            </div>
+            <div className="input">
+                <div className="label"><label>이메일</label></div>
+                <div className="inputbox">
+                    <input onChange={emailCheck} type="email" name="user_email" maxlength="50" placeholder="이메일을 입력하세요." className="search" />
+                    <CheckIcon className="checkIcon"style={{ display: checkEmailDisplay }}/>    
+                    <ClearIcon className="clearIcon" style={{ display: clearEmailDisplay }}/>  
+                </div>
+            </div>
+            <div className="input">
+                <div className="label"><label>이름</label></div>
+                <div className="inputbox">
+                    <input onChange={nameCheck} type="text" name="userName" maxlength="20" placeholder="이름을 입력하세요." className="search" />
+                    <CheckIcon className="checkIcon"style={{ display: checkNameDisplay }}/>    
+                    <ClearIcon className="clearIcon" style={{ display: clearNameDisplay }}/>  
+                </div>
+            </div>
+            <div className="input">
+                <div className="label"><label>닉네임</label></div>
+                <div className="inputbox">
+                    <input onChange={nickNameCheck} type="text" name="user_nickName" maxlength="20" placeholder="닉네임(별명)을 입력하세요." className="search" />
+                    <CheckIcon className="checkIcon"style={{ display: checknickNameDisplay }}/>    
+                    <ClearIcon className="clearIcon" style={{ display: clearnickNameDisplay }}/>  
+                </div>
+            </div>
+            <button onClick={onSubmit} type="submit">가입하기</button>
+        </div>
+    )    
+}
+
+export default SingUpPage;
