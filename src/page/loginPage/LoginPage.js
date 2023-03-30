71d8205a603a3dc5b29943c79475cbc34835c63b
‘minzung’ <108450681+minzung@users.noreply.github.com>
2023-03-30 16:35:07 +0900

    로그인 구현 완료

--- src/page/loginPage/LoginPage.js
+++ src/page/loginPage/LoginPage.js
@@ -11,30 +11,28 @@ const LoginPage = () => {
   const onSubmit = (e) => {
     e.preventDefault();
 
-    if (studentId.trim().length === 0) {
-      alert("학번을 입력하세요");
-      return;
-    } else if (passwd.trim().length === 0) {
-      alert("비밀번호를 입력하세요");
-      return;
-    } else {
-      axios
-        .post("http://localhost:8080/login", {
-          studentId,
-          passwd,
-        })
-        .then((res) => {
-          if (res.data.code === 200) {
-            alert("로그인 되었습니다!");
-            move("/freeboard/list");
-          } else if (res.data.code === 401) {
-            alert("학번이 존재하지 않습니다");
-          } else if (res.data.code === 402) {
-            alert("비밀번호를 다시 확인해주세요");
-          } else {
-          }
-        });
-    }
+    axios
+    .post('http://localhost:8080/login', {
+      studentId,
+      passwd,
+    })
+    .then((response) => {
+      alert(response.data);
+      move("/freeboard/list")
+    })
+    .catch((error) => {
+      if (error.response) {
+        if (error.response.status === 401) {
+          alert('아이디가 존재하지 않습니다.');
+        } else if (error.response.status === 402) {
+          alert('비밀번호가 틀렸습니다.');
+        } else {
+          alert('로그인에 실패했습니다.');
+        }
+      } else {
+        alert('서버에 연결할 수 없습니다.');
+      }
+    });
   };
 
   return (
