fddda3140a37ab2c53d4bfaf4e745f67724a4a15
minseokiim <alstj991226@naver.com>
2023-03-30 19:40:01 +0900

    페이지 추가

--- src/page/mainPage/Footer.js
+++ src/page/mainPage/Footer.js
@@ -1,23 +1,21 @@
 const Footer = () => {
   return (
-    <div>
-      <section className="footer white">
-        <ul className="links">
-          <li>
-            <a href="/page/serviceagreement">서비스 이용약관</a>
-          </li>
-          <li className="privacy">
-            <a href="/page/rules">커뮤니티 이용규칙</a>
-          </li>
-          <li>
-            <a href="/">문의: capstone23m@gmail.com</a>
-          </li>
-          <li className="copyright">
-            <a href="/">© 에브리 한성</a>
-          </li>
-        </ul>
-      </section>
-    </div>
+    <section className="footer white">
+      <ul className="links">
+        <li>
+          <a href="/page/serviceagreement">서비스 이용약관</a>
+        </li>
+        <li className="privacy">
+          <a href="/page/rules">커뮤니티 이용규칙</a>
+        </li>
+        <li>
+          <a href="/">문의: capstone23m@gmail.com</a>
+        </li>
+        <li className="copyright">
+          <a href="/">© 에브리 한성</a>
+        </li>
+      </ul>
+    </section>
   );
 };
 
