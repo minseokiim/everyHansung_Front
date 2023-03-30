1a5a891fb414457105bd4311110d74db49afbb1d
minseokiim <alstj991226@naver.com>
2023-03-30 17:40:07 +0900

    이용약관 추가

--- src/page/mainPage/MainPage.js
+++ src/page/mainPage/MainPage.js
@@ -110,13 +110,13 @@ const MainPage = () => {
       <section className="footer white">
         <ul className="links">
           <li>
-            <a href="/page/serviceagreement">이용약관</a>
+            <a href="/page/serviceagreement">서비스 이용약관</a>
           </li>
           <li className="privacy">
-            <a href="/page/privacy">개인정보처리방침</a>
+            <a href="/page/rules">커뮤니티 이용규칙</a>
           </li>
           <li>
-            <a href="/page/faq">문의하기</a>
+            <a href="/">문의: 010-1234-5678</a>
           </li>
           <li className="copyright">
             <a href="/">© 에브리 한성</a>
