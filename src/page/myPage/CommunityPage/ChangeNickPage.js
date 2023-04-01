f1881421190ccc02dd5ea563779606d55bf389ac
‘minzung’ <108450681+minzung@users.noreply.github.com>
2023-04-02 01:38:12 +0900

    페이지 구현 수정

--- src/page/myPage/CommunityPage/ChangeNickPage.js
+++ src/page/myPage/CommunityPage/ChangeNickPage.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import axios from "axios";
 import "../../signupPage/SignUpPage.css";
 import CheckIcon from "@material-ui/icons/Check";
 import ClearIcon from "@material-ui/icons/Clear";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const ChangeNickPage = () => {
   const [nickname, setNickname] = useState("none");
@@ -11,53 +11,35 @@ const ChangeNickPage = () => {
   const [checkNicknameDisplay, setCheckNicknameDisplay] = useState("none");
   const [clearNicknameDisplay, setClearNicknameDisplay] = useState("none");
 
-  const [checkConfirmNicknameDisplay, setCheckConfirmNicknameDisplay] =
-    useState("none");
-  const [clearConfirmNicknameDisplay, setClearConfirmNicknameDisplay] =
-    useState("none");
-
   const move = useNavigate();
 
   const nicknameCheck = (e) => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    try {
-      if (regex.test(e.target.value)) {
-        setNickname(e.target.value);
-        setCheckNicknameDisplay("block");
-        setClearNicknameDisplay("none");
-      } else {
-        setClearNicknameDisplay("block");
-        setCheckNicknameDisplay("none");
-      }
-    } catch (e) {}
-  };
-
-  const confirmNicknameCheck = (e) => {
-    if (e.target.value !== nickname) {
-      setClearConfirmNicknameDisplay("block");
-      setCheckConfirmNicknameDisplay("none");
+    if (e.target.value.length >= 2) {
+      setNickname(e.target.value);
+      setCheckNicknameDisplay("block");
+      setClearNicknameDisplay("none");
     } else {
-      setCheckConfirmNicknameDisplay("block");
-      setClearConfirmNicknameDisplay("none");
+      setClearNicknameDisplay("block");
+      setCheckNicknameDisplay("none");
     }
   };
 
+
   const onSubmit = (e) => {
-    if (
-      checkNicknameDisplay === "block" &&
-      checkConfirmNicknameDisplay === "block"
-    ) {
+    e.preventDefault();
+
+    if (checkNicknameDisplay === "block") {
       axios
-        .patch("http://localhost:8080/my/nickname", {
-          nickname,
+        .patch(`http://localhost:8080/nickname/${nickname}`, {
+          // studentId, ${studentId}
+          nickname
         })
         .then(() => {
-          alert("이메일 변경하였습니다.");
+          alert("닉네임 변경하였습니다.");
           move("/my");
         })
         .catch((error) => {
-          alert("이메일 변경에 실패하셨습니다.");
+          alert("닉네임 변경에 실패하셨습니다.");
         });
     }
   };
@@ -66,28 +48,7 @@ const ChangeNickPage = () => {
     <div id="register-container">
       <div className="">
         <div className="mb-3 ">
-          <strong>에브리한성 닉네임 변경</strong>
-
-          <div className="input">
-            <div className="inputbox">
-              <input
-                onChange={nicknameCheck}
-                type="text"
-                name="user_nickName"
-                maxLength="20"
-                placeholder="닉네임(별명)을 입력하세요."
-                className="search"
-              />
-              <CheckIcon
-                className="checkIcon"
-                style={{ display: checkNicknameDisplay }}
-              />
-              <ClearIcon
-                className="clearIcon"
-                style={{ display: clearNicknameDisplay }}
-              />
-            </div>
-          </div>
+          <strong>닉네임 설정</strong>
 
           <div className="input">
             <div className="inputbox">
