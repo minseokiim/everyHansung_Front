d9d249862e76e7c19427c4d2ec883120fefba2d8
‘minzung’ <108450681+minzung@users.noreply.github.com>
2023-04-17 14:42:13 +0900

    id가 undefined 오류 수정 필요

--- src/page/freeBoardPage/Heart/FreeBoardHeart.js
+++ src/page/freeBoardPage/Heart/FreeBoardHeart.js
@@ -2,26 +2,68 @@ import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
+import apiClient from "../../../apiClient";
 
 const FreeBoardHeart = () => {
   const [isFilled, setIsFilled] = useState(false);
   const [countLike, setCountLike] = useState(0);
-  const { id } = useParams();
+  const studentId = localStorage.getItem("studentId");
+  const { freeboardId } = useParams();
+  const [message, setMessage] = useState('');
 
-  const clickHeart = () => {
-    if (!isFilled) {
-      setIsFilled(true);
-      setCountLike(countLike + 1);
-
-      //좋아요 수 patch
-      axios.patch(`http://localhost:8080/freeboard/${id}`, {
-        countLike,
+  const clickHeart = async () => {
+    try {
+      console.log("freeboardId = " + freeboardId);
+      const response = await apiClient.post(`http://localhost:8080/heart`, null, {
+        params: {
+          memberId: studentId,
+          freeboardId: freeboardId
+        }
       });
-    } else {
-      setIsFilled(false);
-      setCountLike(countLike - 1);
+      if (response.status === 201) {
+        setMessage('좋아요가 추가되었습니다.');
+      } else {
+        setMessage('좋아요 추가에 실패했습니다.');
+      }
+    } catch (error) {
+      if (error.response.status === 400) {
+        setMessage(`Error: ${error.response.data}`);
+      } else {
+        setMessage(`Error: ${error.message}`);
+      }
     }
   };
+
+
+  // const clickHeart = () => {
+  //   if (!isFilled) {
+  //     setIsFilled(true);
+  //     setCountLike(countLike + 1);
+
+  //     //좋아요 수 patch
+  //     apiClient.post(`http://localhost:8080/hearts`, null, {
+  //       params: {
+  //           memberId: studentId,
+  //           freeboardId: id
+  //       }
+  //   })
+  //   .then(response => {
+  //       // 요청이 성공한 경우 처리
+  //   })
+  //   .catch(error => {
+  //       if (error.response.status === 400) {
+  //           // 400 오류 발생 시 처리
+  //           console.log("Error:", error.response.data);
+  //       } else {
+  //           // 다른 오류 발생 시 처리
+  //           console.log("Error:", error.message);
+  //       }
+  //   });
+  //   } else {
+  //     setIsFilled(false);
+  //     setCountLike(countLike - 1);
+  //   }
+  // };
 
   return (
     <div>
