91a911bda8113ac02f6bf9c7d2ad4b5e1898231c
minseokiim <alstj991226@naver.com>
2023-04-21 18:05:13 +0900

    대댓글 기능 추가, ENCODING 201 오류 수정 전

--- src/page/freeBoardPage/Comment/FreeCommentListPage.js
+++ src/page/freeBoardPage/Comment/FreeCommentListPage.js
@@ -6,6 +6,7 @@ import { BsFillTrashFill } from "react-icons/bs";
 import FreeCommentWritePage from "./FreeCommentWritePage";
 import { FaRegCommentDots } from "react-icons/fa";
 import { AiOutlineComment } from "react-icons/ai";
+import FreeReplyCommentPage from "./FreeReplyCommentPage";
 
 const FreeCommentListPage = () => {
   const [comment, setComment] = useState([]);
@@ -18,7 +19,6 @@ const FreeCommentListPage = () => {
     apiClient
       .get(`http://localhost:8080/freeboard/comment/${id}`)
       .then((res) => {
-        //console.log(res.data);
         setComment(res.data);
       });
   };
@@ -44,8 +44,23 @@ const FreeCommentListPage = () => {
       });
   };
 
+  const toggleReplyForm = (commentId) => {
+    setShowReplyForm((prev) => (prev === commentId ? null : commentId));
+  };
+
+  // const postReply = (parentId, reply) => {
+  //   apiClient
+  //     .post(`http://localhost:8080/freeboard/comment/${parentId}/replies`, {
+  //       ...reply,
+  //       studentId,
+  //     })
+  //     .then(() => {
+  //       refetchComments();
+  //     });
+  // };
+
   return (
-    <>
+    <div>
       <div>
         <AiOutlineComment />
         <span className="p-1">
@@ -58,45 +73,70 @@ const FreeCommentListPage = () => {
               .filter((comment) => comment.content !== 0)
               .map((comment) => {
                 return (
-                  <div className="d-flex" key={comment.id}>
-                    <div className="comment-box flex-grow-1">
-                      {comment.isAnonymous ? "익명" : comment.nickname}:
-                      <span className="p-1">{comment.content}</span>
-                      <div className="comment-time ">
-                        {printDate(comment.createdAt)}
+                  <div key={comment.id}>
+                    <div className="d-flex">
+                      <div className="comment-box flex-grow-1">
+                        {comment.isAnonymous ? "익명" : comment.nickname}:
+                        <span className="p-1">{comment.content}</span>
+                        <div className="comment-time ">
+                          {printDate(comment.createdAt)}
+                        </div>
                       </div>
-                      <hr />
-                    </div>
-                    <div>
-                      {/* 대댓글 다는 기능으로 바꾸기 */}
-                      <FaRegCommentDots
-                        className="cursor-pointer icon"
-                        // onClick={() =>
-                        //   setShowReplyForm(
-                        //     showReplyForm === comment.id ? null : comment.id
-                        //   )
-                        // }
-                      />
+                      <div>
+                        {/* 대댓글 다는 기능으로 바꾸기 */}
+                        <FaRegCommentDots
+                          className="cursor-pointer icon  grey"
+                          onClick={() => toggleReplyForm(comment.id)}
+                        />
 
-                      {studentId === comment.studentId && (
-                        <>
-                          <span className="p-2">
-                            <BsFillTrashFill
-                              className="cursor-pointer icon"
-                              onClick={(e) => {
-                                {
-                                  if (
-                                    window.confirm("게시물을 삭제하시겠습니까?")
-                                  ) {
-                                    deleteComment(e, id, comment.id);
+                        {studentId === comment.studentId && (
+                          <>
+                            <span className="p-2">
+                              <BsFillTrashFill
+                                className="cursor-pointer icon grey"
+                                onClick={(e) => {
+                                  {
+                                    if (
+                                      window.confirm(
+                                        "게시물을 삭제하시겠습니까?"
+                                      )
+                                    ) {
+                                      deleteComment(e, id, comment.id);
+                                    }
                                   }
-                                }
-                              }}
-                            />
-                          </span>
-                        </>
-                      )}
+                                }}
+                              />
+                            </span>
+                          </>
+                        )}
+                      </div>
                     </div>
+                    <hr />
+                    {/* 대댓글 창 */}
+                    {showReplyForm === comment.id && (
+                      <div className="reply-form">
+                        <FreeReplyCommentPage
+                          parentId={comment.id}
+                          postReply={refetchComments()}
+                        />
+                      </div>
+                    )}
+                    {/* 대댓글 추가되면 보이는 대댓글 리스트 ->현재 안됨*/}
+                    {comment.replies &&
+                      comment.replies.map((reply) => (
+                        <div key={reply.id} className="nested-reply">
+                          <div className="d-flex">
+                            <div className="comment-box flex-grow-1">
+                              {reply.isAnonymous ? "익명" : reply.nickname}:
+                              <span className="p-1">{reply.content}</span>
+                              <div className="comment-time ">
+                                {printDate(reply.createdAt)}
+                              </div>
+                            </div>
+                          </div>
+                          <hr />
+                        </div>
+                      ))}
                   </div>
                 );
               })
@@ -110,8 +150,9 @@ const FreeCommentListPage = () => {
       </div>
       <br />
       <br />
+      {/* 일반 댓글창 */}
       <FreeCommentWritePage refetchComments={refetchComments} />
-    </>
+    </div>
   );
 };
 
