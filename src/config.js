f83915c9868f6aab1c729db550e1378d3c01e26f
minseokiim <55376275+minseokiim@users.noreply.github.com>
2023-05-26 00:15:49 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://43.201.168.217:8080";
+  process.env.REACT_APP_API_BASE_URL || "고정ip:8080";
 
 export default {
   API_BASE_URL,
